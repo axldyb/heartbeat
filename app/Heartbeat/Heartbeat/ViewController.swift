2597c62229e4b580a7bad4adacc1fd5e42b6d272
Aksel Dybdal <akseldybdal@gmail.com>
2021-11-06 17:29:45 +0100

    Add iOS app + proto gen

---
+++ app/Heartbeat/Heartbeat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heartbeat
+//
+//  Created by Aksel Dybdal on 06/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- proto/generate-types.sh
+++ proto/generate-types.sh
@@ -4,9 +4,14 @@ echo "Generating types..."
 
 rm -rf ../client/proto
 rm -rf ../server/proto
+rm -rf ../app/Heartbeat/Heartbeat/proto
+
+mkdir ../app/Heartbeat/Heartbeat/proto
 
 ./node_modules/.bin/proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=../client/proto ./*.proto
 ./node_modules/.bin/proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=../server/proto ./*.proto
+protoc --plugin=./swift/protoc-gen-swift heartbeat.proto --swift_out=../app/Heartbeat/Heartbeat/proto
+protoc --plugin=./swift/protoc-gen-grpc-swift heartbeat.proto --swift_out=../app/Heartbeat/Heartbeat/proto --grpc-swift_out=Client=true,Server=false:../app/Heartbeat/Heartbeat/proto
 
 cp ./*.proto ../client/proto
 cp ./*.proto ../server/proto
