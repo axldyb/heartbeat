314e1c3917eeb3e4076db04ac6485fc9420a7748
Aksel Dybdal <akseldybdal@gmail.com>
2021-11-29 22:22:54 +0100

    Change initial heartbeats table migration

---
+++ server/db/migrations/20211129220413_create_heartbeats_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('heartbeats', function (table) {
+        table.uuid('id').primary().defaultTo(knex.raw("gen_random_uuid()"))
+        table.timestamp('timestamp').defaultTo(knex.fn.now())
+        table.string('client_id', 255).notNullable()
+        table.string('client_name', 255).notNullable()
+        table.string('client_version', 255).notNullable()
+        table.string('device_id', 255).notNullable()
+        table.string('device_name', 255).notNullable()
+        table.string('device_os', 255).notNullable()
+        table.string('device_os_version', 255).notNullable()
+        table.string('device_model', 255).notNullable()
+        table.string('device_language', 255).notNullable()
+    })
+
+}
+
+exports.down = function (knex, Promise) {
+
+}
