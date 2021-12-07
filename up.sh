5cfe0fd9cfe320e30fed1579481e7b0ff56312c4
Aksel Dybdal <akseldybdal@gmail.com>
2021-12-07 21:06:15 +0100

    Load .env in up script

--- up.sh
+++ up.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ "$NODE_ENV" == "production" ] ; then
-  echo "Starting docker compose in production mode"
-  docker-compose -f docker-compose.yml up -d
-else
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+if [ "$NODE_ENV" = "development" ] ; then
   echo "Starting docker compose in development mode"
   docker-compose -f docker-compose.yml -f docker-compose.development.yml up -d
+else
+  echo "Starting docker compose in production mode"
+  docker-compose -f docker-compose.yml up -d
 fi
