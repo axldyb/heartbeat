5f364fb0c86443b97c2b46ba16ecb318b2ee039e
Aksel Dybdal <akseldybdal@gmail.com>
2021-12-24 11:10:29 +0100

    Update gRPC types generation script to support the new Swift package

--- proto/generate-types.sh
+++ proto/generate-types.sh
@@ -4,14 +4,28 @@ echo "Generating types..."
 
 rm -rf ../rest-api/proto
 rm -rf ../server/proto
-rm -rf ../app/Heartbeat/Heartbeat/proto
+rm -rf ../app/HeartbeatKit/Sources/HeartbeatKit/proto
 
-mkdir ../app/Heartbeat/Heartbeat/proto
+mkdir ../app/HeartbeatKit/Sources/HeartbeatKit/proto
 
 ./node_modules/.bin/proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=../rest-api/proto ./*.proto
 ./node_modules/.bin/proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=../server/proto ./*.proto
-protoc --plugin=./swift/protoc-gen-swift heartbeat.proto --plugin=protoc-gen-grpc=./swift/protoc-gen-swift --experimental_allow_proto3_optional --swift_out=../app/Heartbeat/Heartbeat/proto
-protoc --plugin=./swift/protoc-gen-grpc-swift heartbeat.proto --plugin=protoc-gen-grpc=./swift/protoc-gen-swift --plugin=protoc-gen-grpc=./swift/protoc-gen-grpc-swift --experimental_allow_proto3_optional --swift_out=../app/Heartbeat/Heartbeat/proto --grpc-swift_out=Client=true,Server=false:../app/Heartbeat/Heartbeat/proto
+
+protoc heartbeat.proto \
+    --plugin=protoc-gen-grpc=./swift/protoc-gen-swift \
+    --plugin=./swift/protoc-gen-swift \
+    --experimental_allow_proto3_optional \
+    --swift_opt=Visibility=Public \
+    --swift_out=../app/HeartbeatKit/Sources/HeartbeatKit/proto
+
+protoc heartbeat.proto \
+    --plugin=./swift/protoc-gen-grpc-swift \
+    --plugin=protoc-gen-grpc=./swift/protoc-gen-swift \
+    --plugin=protoc-gen-grpc=./swift/protoc-gen-grpc-swift \
+    --experimental_allow_proto3_optional \
+    --swift_out=../app/HeartbeatKit/Sources/HeartbeatKit/proto \
+    --swift_opt=Visibility=Public \
+    --grpc-swift_out=Client=true,Server=false:../app/HeartbeatKit/Sources/HeartbeatKit/proto
 
 cp ./*.proto ../rest-api/proto
 cp ./*.proto ../server/proto
