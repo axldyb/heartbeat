d2a8560037febc91076b94055f2b0243a0924d5f
Aksel Dybdal <akseldybdal@gmail.com>
2022-01-17 10:21:22 +0100

    Add cors function

--- rest-api/src/app.ts
+++ rest-api/src/app.ts
@@ -6,13 +6,27 @@ import { RouteLoggerMiddleware } from './utils/route-logger-middleware'
 const routeLogger = new RouteLoggerMiddleware()
 const cors = require('cors')
 
+const allowedOrigins = ['http://localhost:4200']
 
 // express
 const app = express()
-app.use(cors)
 app.use(routeLogger.middlewareFunc)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true)
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.'
+            return callback(new Error(msg), false)
+        }
+        return callback(null, true)
+    }
+}))
 
 // routes
 app.use('/api', heartbeatsRouter)
